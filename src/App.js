e0b4ed0ae443102fd3abe48b9ffaa650dfccdf73
zigsong <wldms5764@gmail.com>
2021-04-29 09:59:02 +0900

    chore: settings for cra & chakra UI

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <p>Visualization team B</p>
+    </ChakraProvider>
+  );
+}
+
+export default App;
